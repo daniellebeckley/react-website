203c05cca806985c6d86e45680dbfb6861445cca
Danielle Beckley <db556v@att.com>
2024-01-26 11:11:06 -0600

    Making things pretty

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,17 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="red">
-          Aside<GenreList></GenreList>
+        <GridItem area="aside" bg="red" paddingX={5}>
+          <GenreList></GenreList>
         </GridItem>
       </Show>
       <GridItem area="main">
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,31 @@
 import useGenres from "../hooks/useGenres";
-import { HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { HStack, Heading, Image, List, ListItem, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/Image-url";
 
 const GenreList = () => {
   const { data: genres, error, loading } = useGenres();
   return (
     <>
-      {genres.map((genre) => (
-        <HStack key={genre.id}>
-          <Image
-            key={genre.name}
-            paddingBlock={1}
-            height={10}
-            width={10}
-            src={getCroppedImageUrl(genre.image_background)}
-            alt={genre.name}
-          />
-          <Text key={genre.id}> {genre.name} </Text>
-        </HStack>
-      ))}
+      <List>
+        {genres.map((genre) => (
+          <ListItem paddingY="5px">
+            <HStack key={genre.id}>
+              <Image
+                key={genre.name}
+                paddingBlock={1}
+                borderRadius={8}
+                boxSize="42px"
+                src={getCroppedImageUrl(genre.image_background)}
+                alt={genre.name}
+              />
+              <Text fontSize="lg" key={genre.id}>
+                {" "}
+                {genre.name}{" "}
+              </Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
