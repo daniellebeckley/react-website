12adf84a0f264aca3a003f9ba590c70ddbac8f4e
dani beckley <dani.beckley@slalom.com>
2024-02-05 15:23:26 -0600

    Build search input

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -40,13 +40,13 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
                 alt={genre.name}
               />
               //change this to a button that logs the genre (or the function)
-              <Button
+              <Text
                 fontSize="lg"
                 key={genre.id}
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               >
                 {genre.name}
-              </Button>
+              </Text>
             </HStack>
           </ListItem>
         ))}
