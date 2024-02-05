c429bf75b6c306cf4e056bad86f3492535201af1
dani beckley <dani.beckley@slalom.com>
2024-02-05 15:16:32 -0600

    Handle games with no image

--- src/services/Image-url.ts
+++ src/services/Image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
   const media = "media/";
-  if (url === null) return "";
+  if (url === null) return noImage;
   const index = url.indexOf(media) + media.length;
 
   return url.slice(0, index).concat("crop/600/400/").concat(url.slice(index));
