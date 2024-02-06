d1ae95a936d0e4aa0079bbe52d3da88a7f5d41b7
dani beckley <dani.beckley@slalom.com>
2024-02-06 15:26:01 -0600

    statically load platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,4 @@
-import useData from "./useData";
-import { Platform } from "./useGames";
+import platforms from "../data/platform";
 
-const usePlatforms = () => useData<Platform>("platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 export default usePlatforms;
