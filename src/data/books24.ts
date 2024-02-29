48129e1717843258306014b5a7a056c680976348
Danielle Smith <dbeckley.52@gmail.com>
2024-02-29 08:20:21 -0600

    Update books24.ts

--- src/data/books24.ts
+++ src/data/books24.ts
@@ -20,4 +20,17 @@ export default [
     img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1676671937i/112975658.jpg",
     audiobook: true,
   },
+  {
+  title: "Killers of the Flower Moon",
+  author: "David Grann",
+  rating: 4.5,
+  img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1696884539i/193388249.jpg",
+  audiobook: true,
+  },
+  {
+  title: "Revenge (short story)",
+  author: "Jim Harrison",
+  rating: 4.5,
+  img: "https://m.media-amazon.com/images/I/51cDUdPnUiL._SL350_.jpg",
+  },
 ];
